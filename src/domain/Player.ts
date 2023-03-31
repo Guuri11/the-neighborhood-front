fe41489c1d1f2ee96c9a4072a94dc9d1723db0c7
Sergio Gurillo <sergio.gurillo11@gmail.com>
2023-03-31 18:21:48 +0200

    sign up assets

---
+++ src/domain/Player.ts
@@ -0,0 +1,3 @@
+export type Gender = "male" | "female" | "other";
+export type  Position = "POINT_GUARD" | "SHOOTING_GUARD" | "SMALL_FORWARD" | "POWER_FORWARD" | "CENTER";
+export type Archetype = "SLASHER"
