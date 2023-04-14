8eefc5acc1880ff776680adafbd93dcb4a484711
Sergio Gurillo <sergio.gurillo11@gmail.com>
2023-04-14 21:27:49 +0200

    ui, notification & loading store

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import AppWrapper from "./src/components/core/AppWrapper";
 import Security from "./src/components/core/Security";
+import Notifications from "./src/components/design/layout/Notifications/Notifications";
 
 export default function App() {
   return (
     <AppWrapper>
       <Security />
+      <Notifications />
     </AppWrapper>
   );
 }
